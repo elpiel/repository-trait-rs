18bd668b36f8c8bbee6b5456a1e78da26d754bfc
Lachezar Lechev <elpiel93@gmail.com>
2019-05-14 20:12:01 +0300

    infrastructure - persistence: - add `DbPool` type - impl PostgresChannelRepository

--- src/infrastructure/persistence/channel.rs
+++ src/infrastructure/persistence/channel.rs
@@ -3,6 +3,7 @@ use futures_legacy::Future as LegacyFuture;
 
 use crate::domain::{RepositoryError, RepositoryFuture};
 use crate::domain::channel::{Channel, ChannelRepository};
+use crate::infrastructure::persistence::DbPool;
 
 pub struct MemoryChannelRepository {
     records: Vec<Channel>,
@@ -22,6 +23,50 @@ impl ChannelRepository for MemoryChannelRepository {
             )
         )
     }
+
+    fn insert(&self, channel: Channel) -> RepositoryFuture<()> {
+        Box::pin(
+            futures::future::ok(
+                ()
+            )
+        )
+    }
+}
+
+
+pub struct PostgresChannelRepository {
+    pool: DbPool,
+}
+
+impl PostgresChannelRepository {
+    pub fn new(pool: DbPool) -> Self {
+        Self { pool }
+    }
+}
+
+impl ChannelRepository for PostgresChannelRepository {
+    fn list(&self) -> RepositoryFuture<Vec<Channel>> {
+        let mut conn = self.pool.get().unwrap();
+        let stmt = conn.prepare("SELECT channel_id FROM channels").unwrap();
+
+        let results = conn
+            .query(&stmt, &[])
+            .unwrap()
+            .iter()
+            .map(|row| {
+                Channel {
+                    id: row.get("channel_id")
+                }
+            })
+            .collect();
+
+        Box::pin(
+            futures::future::ok(
+                results
+            )
+        )
+    }
+
     fn insert(&self, channel: Channel) -> RepositoryFuture<()> {
         Box::pin(
             futures::future::ok(
